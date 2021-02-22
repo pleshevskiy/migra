7dc1477f1f1cbdf1a2ea056c7ef47f0cff49162d
Dmitriy Pleshevskiy <dmitriy@ideascup.me>
2021-02-22 16:27:27 +0300

    chore: update migration content for sample db

---
+++ sample/database/migrations/210206002058_create_tag/up.sql
@@ -0,0 +1,7 @@
+-- Your SQL goes here
+
+CREATE TABLE tags (
+    id          serial          PRIMARY KEY,
+    name        text            NOT NULL UNIQUE CHECK(length(name) > 0),
+    created_at  timestamp       NOT NULL DEFAULT current_timestamp
+);
